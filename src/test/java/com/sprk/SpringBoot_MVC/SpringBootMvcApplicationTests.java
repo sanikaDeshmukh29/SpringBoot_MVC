9e7afdfc54b0cb2c9ae777b71aaf7f71611eef0c
sanikaDeshmukh29 <sanikadesh29@gmail.com>
2025-08-16 22:18:56 +0530

    Initial commit

---
+++ src/test/java/com/sprk/SpringBoot_MVC/SpringBootMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprk.SpringBoot_MVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
