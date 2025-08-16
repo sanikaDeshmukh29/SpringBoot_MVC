e5747fc9ecf94de844f3f2312571a250f381bc7d
sanikaDeshmukh29 <sanikadesh29@gmail.com>
2025-08-16 23:18:47 +0530

    displaying student object

--- src/main/java/com/sprk/SpringBoot_MVC/controller/MVCController.java
+++ src/main/java/com/sprk/SpringBoot_MVC/controller/MVCController.java
@@ -1,6 +1,8 @@
 package com.sprk.SpringBoot_MVC.controller;
 
+import com.sprk.SpringBoot_MVC.entity.Student;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,5 +11,23 @@ public class MVCController {
     @GetMapping("/demo")
     public String showDemo(){
         return "demo";
+    }
+
+    @GetMapping("/show-student")
+    public String showStudent(Model model){
+
+        Student student = new Student();
+
+        student.setRollNo(10);
+        student.setFirstName("John");
+        student.setLastName("Doe");
+        student.setEmail("john@gmail.com");
+        student.setPhone("+91 12345 12345");
+        student.setGender("Male");
+        student.setPrime(true);
+
+
+        model.addAttribute("student",student);
+        return "student-information";
     }
 }
